de27402cca0f55d0dd4e28369f2d7c54e3664335
WangHexie <xuanyangfang@qq.com>
2018-04-30 00:25:53 +0800

    add hash_to_chinese_key func

--- Server/database.py
+++ Server/database.py
@@ -1,7 +1,13 @@
-from .basic_function import *
+from Server import basic_function
+
 
 def get_doc_from_database(key):
     return 0
 
-def store_doc_to_database(key):
+
+def store_doc_to_database(doc):
     return 0
+
+
+if __name__ == '__main__':
+    print(basic_function.create_key())
