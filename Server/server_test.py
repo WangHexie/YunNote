8b6738ad61e46c62c55cec5e3b10dada15fe1a7f
WangHexie <xuanyangfang@qq.com>
2018-04-30 19:49:24 +0800

    somethin

--- Server/server_test.py
+++ Server/server_test.py
@@ -1,15 +1,17 @@
 import requests
 
+# server = "http://localhost:5000/"
+server = "http://ipv4.dfen.xyz:5000/"
 # Test Store
 json1 = {'doc': 'oldpig'}
-response = requests.post("http://ipv4.dfen.xyz:5000/store", params=json1)
+response = requests.post(server + "store", params=json1)
 print(response.text)
 
 # Test Get
 json2 = {'key': '6c253648e0dfff00d6aa44566f34dae651f0d2191d5bf59dd74d8ce7314cb3e7'}
-response = requests.get("http://ipv4.dfen.xyz:5000/get", params=json2)
+response = requests.get(server+"get", params=json2)
 print(response.text)
 
 # Test Server Connection
-response = requests.get("http://ipv4.dfen.xyz:5000/")
+response = requests.get(server)
 print(response.text)
