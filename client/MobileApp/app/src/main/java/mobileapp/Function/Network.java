8309cdd69d4956aa7176b18ec5a2641fcd53d0d1
WangHexie <xuanyangfang@qq.com>
2018-05-09 14:16:01 +0800

    add sotre and get cnkey method to Network

--- client/MobileApp/app/src/main/java/mobileapp/Function/Network.java
+++ client/MobileApp/app/src/main/java/mobileapp/Function/Network.java
@@ -29,6 +29,28 @@ public class Network {
         return key;
     }
 
+    public static String setDocAndGetCnKey(String doc) {
+        String encodedUrl = "";
+        try {
+            encodedUrl = java.net.URLEncoder.encode(doc, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String key = sendPost(url + "storeck", "doc=" + encodedUrl);
+        return key;
+    }
+
+    public static String getDocByCnKey(String cnkey) {
+        String encodedUrl = "";
+        try {
+            encodedUrl = java.net.URLEncoder.encode(cnkey, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String key = sendGet(url + "getbyck", "cnkey=" + encodedUrl);
+        return key;
+    }
+
     public static String addDocToList(String doc, String cookies) {
         String encodedUrl = "";
         try {
@@ -219,16 +241,19 @@ public class Network {
     }
 
     public static void main(String[] args) {
+//        print(setDocAndGetCnKey("123"));
+        print(getDocByCnKey("国克儿行员"));
 //        System.out.println(getDoc("6c253648e0dfff00d6aa44566f34dae651f0d2191d5bf59dd74d8ce7314cb3e7"));
 //        System.out.println(setDoc("23333"));
 //        System.out.println(addDocToList("big 2","002e7e436441939758c41ed393257d2cea503d0dfb234201af1bfc21bfbc6d55"));
 //        print(deleteDoc("d1e9052456c98c1b624b31def8aea3e3fdaf5e578825b13ac7035dda9cd9631c","002e7e436441939758c41ed393257d2cea503d0dfb234201af1bfc21bfbc6d55"));
 //        print(ifNameExist("1234"));
 //        print(signIn("1234", "123"));
-        String li = getList("b4373a57ae6b094ab2e9837fe2a79f1f247dd2bfb04083f6aba15a0d90b2cf4c");
-        print(StringReformat.toDocList(li));
-        print(StringReformat.toKeyList(li));
-        print(Network.deleteDoc("17980a24eac0a090325295ebd5e7ddf2d2d9e1d1104b1f9e4f4e2fa8c6e8429b", "b4373a57ae6b094ab2e9837fe2a79f1f247dd2bfb04083f6aba15a0d90b2cf4c"));
+//        String li = getList("b4373a57ae6b094ab2e9837fe2a79f1f247dd2bfb04083f6aba15a0d90b2cf4c");
+//        print(StringReformat.toDocList(li));
+//        print(StringReformat.toKeyList(li));
+//        print(Network.deleteDoc("17980a24eac0a090325295ebd5e7ddf2d2d9e1d1104b1f9e4f4e2fa8c6e8429b", "b4373a57ae6b094ab2e9837fe2a79f1f247dd2bfb04083f6aba15a0d90b2cf4c"));
 //        print(Network.addDocToList("狗蛋" ,"002e7e436441939758c41ed393257d2cea503d0dfb234201af1bfc21bfbc6d55"));
+
     }
 }
